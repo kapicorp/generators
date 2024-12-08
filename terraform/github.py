4623ad4df92298fe82a43a41c44e7e0f8d3b79d3
Alessandro De Maria <ademaria@isomorphiclabs.com>
2024-12-08 14:20:27 +0000

    upgrade generators

--- terraform/github.py
+++ terraform/github.py
@@ -18,6 +18,9 @@ class GenGitHubRepository(TerraformStore):
         tag_protection_config = config.pop("tag_protection", {})
         deploy_keys_config = config.pop("deploy_keys", {})
         ruleset_config = config.pop("repository_ruleset", {})
+        actions_config = config.pop("actions", {})
+        access_permissions_config = config.pop("permissions", {})
+        autolink_references_config = config.pop("autolink", {})
 
         resource_name = self.name
         logger.debug(f"Processing github_repository {resource_name}")
@@ -43,6 +46,9 @@ class GenGitHubRepository(TerraformStore):
             branch_protection.filename = "github_branch_protection.tf"
             branch_protection.set(branch_protection.config)
             branch_protection.add("repository_id", repository.get_reference("node_id"))
+            branch_protection.set(
+                {"pattern": branches_name}
+            )  # Ensures the pattern is unique to the branch name and doesn't default to `main`
             self.add(branch_protection)
 
         for rule_name, tag_pattern in tag_protection_config.items():
@@ -84,3 +90,54 @@ class GenGitHubRepository(TerraformStore):
             repository_ruleset.filename = "github_repository_ruleset.tf"
             repository_ruleset.set(repository_ruleset.config)
             self.add(repository_ruleset)
+
+        if actions_config.get("access_level") is not None:
+            gha_actions_access = TerraformResource(
+                id=f"{resource_id}_actions_access",
+                type="github_actions_repository_access_level",
+                config={
+                    "repository": repository.get_reference("name"),
+                    "access_level": actions_config.get("access_level"),
+                },
+            )
+            gha_actions_access.filename = "github_repository_actions.tf"
+            gha_actions_access.set(gha_actions_access.config)
+            gha_actions_access.add("repository", repository.get_reference("name"))
+            self.add(gha_actions_access)
+
+        for permission_type, permission_config in access_permissions_config.items():
+            logger.debug(f"Processing permissions for {resource_name}")
+            for entity, permission in permission_config.items():
+                if permission_type == "team":
+                    config = {"team_id": f"{entity}", "permission": f"{permission}"}
+                else:
+                    config = {"username": f"{entity}", "permission": f"{permission}"}
+                repository_collaborators = TerraformResource(
+                    id=f"{resource_name}_access_permissions".replace(".", ""),
+                    type="github_repository_collaborators",
+                    config=config,
+                )
+                repository_collaborators.filename = "github_repository_collaborators.tf"
+                repository_collaborators.add(
+                    "repository", repository.get_reference("name")
+                )
+                repository_collaborators.add(
+                    permission_type, [repository_collaborators.config]
+                )
+                self.add(repository_collaborators)
+
+        for key_prefix, target_url in autolink_references_config.items():
+            logger.debug(f"Processing autolink referneces for {resource_name}")
+            config = {
+                "key_prefix": f"{key_prefix}-",
+                "target_url_template": f"{target_url}",
+            }
+            autolink_references = TerraformResource(
+                id=f"{key_prefix}".replace(".", ""),
+                type="github_repository_autolink_reference",
+                config=config,
+            )
+            autolink_references.filename = "github_repository_autolink_reference.tf"
+            autolink_references.set(autolink_references.config)
+            autolink_references.add("repository", repository.get_reference("name"))
+            self.add(autolink_references)
