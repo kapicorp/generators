7dc611fe15da9c0adb7ce27a890f38eb54a85c90
Bruno Almeida <b.almeida@synthace.com>
2025-05-05 12:22:48 -0300

    add FrontendConfig, FrontendConfigGenerator, BackendConfigGenerator

--- kubernetes/gke.py
+++ kubernetes/gke.py
@@ -2,7 +2,8 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-from .common import KubernetesResource
+from .common import KubernetesResource, kgenlib
+from typing import Any
 
 
 class BackendConfig(KubernetesResource):
@@ -11,4 +12,41 @@ class BackendConfig(KubernetesResource):
 
     def body(self):
         super().body()
-        self.root.spec = self.config.backend_config
+        spec = self.spec
+        self.root.spec = spec
+
+
+@kgenlib.register_generator(path="generators.kubernetes.backend_config")
+class BackendConfigGenerator(kgenlib.BaseStore):
+    name: str
+    config: Any
+
+    def body(self):
+        name = self.name
+        config = self.config
+        spec = self.config.spec
+        backend_config = BackendConfig(name=name, config=config, spec=spec)
+        self.add(backend_config)
+
+
+class FrontendConfig(KubernetesResource):
+    kind: str = "FrontendConfig"
+    api_version: str = "networking.gke.io/v1beta1"
+
+    def body(self):
+        super().body()
+        spec = self.spec
+        self.root.spec = spec
+
+
+@kgenlib.register_generator(path="generators.kubernetes.frontend_config")
+class FrontendConfigGenerator(kgenlib.BaseStore):
+    name: str
+    config: Any
+
+    def body(self):
+        name = self.name
+        config = self.config
+        spec = self.config.spec
+        frontend_config = FrontendConfig(name=name, config=config, spec=spec)
+        self.add(frontend_config)
